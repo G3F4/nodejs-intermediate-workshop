0f67f7380898cd8b6be4841337af44c001207957
Kamil Grabek <kamil.grabek@allegro.pl>
2019-04-03 12:58:40 +0200

    strategia dla passport-github - dynamiczna

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ passport.deserializeUser(function(obj, cb) {
 });
 
 passport.use(new GitHubStrategy({
-    clientID: '7fb8d0a83343f23179f1',
-    clientSecret: 'f0cba3f5c1832e8acb09a55117c18ebdd4d99bd4',
-    callbackURL: 'http://localhost:5000/auth/github/callback'
+    clientID: process.env.DB === 'remote' ? 'c2c0e9aa52e3be3c9314' : '7fb8d0a83343f23179f1',
+    clientSecret: process.env.DB === 'remote' ? '9bede45ff253717428e0a02fc6c3f6d11d8a1da0' : 'f0cba3f5c1832e8acb09a55117c18ebdd4d99bd4',
+    callbackURL: process.env.DB === 'remote' ? 'https://warsawjs-workshop-31.herokuapp.com/auth/github/callback' : 'http://localhost:5000/auth/github/callback'
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOneAndUpdate({ id: profile.id }, { $set: { id: profile.id }}, { upsert: true  }, function (err, user) {
