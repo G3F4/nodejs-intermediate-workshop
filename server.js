979adf4703d8cd67d9a3798dfcb706f19f74aacc
Kamil Grabek <kamil.grabek@allegro.pl>
2019-04-06 16:06:30 +0200

    dodanie cors i dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -6,6 +7,7 @@ const { PORT } = require('./constans');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 
